c8269fdd7a375041cdec3931d269085d8c4096bf
andmos <andreas.mosti@gmail.com>
2013-01-26 15:48:40 +0100

    mod

--- src/mostiBot.java
+++ src/mostiBot.java
@@ -1,23 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author andreasmosti
  */
 
 import org.jibble.pircbot.*;
+import java.util.ArrayList; 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
 
 public class mostiBot extends PircBot {
+    
+    ArrayList<String> noList = new ArrayList<String>();
+    
     public mostiBot(){
         this.setName("mostiBot");
     }
     
-    public void onMessage(String channel, String sender,
-                       String login, String hostname, String message) {
-        if (message.equalsIgnoreCase("time")) {
+    public void onMessage(String channel, String sender, String login, String hostname, String message) {
+        
+           if (message.equalsIgnoreCase("time")) {
             String time = new java.util.Date().toString();
             sendMessage(channel, sender + ": The time is now " + time);
         }
