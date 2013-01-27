031efdc831f060e0160b9e0a9bf89923b72e3290
andreas mosti <andreas@apollo.tihlde.org>
2013-01-27 13:51:16 +0100

    Added funcitons x

--- src/mostiBot.java
+++ src/mostiBot.java
@@ -1,7 +1,11 @@
 /**
  *
  * @author andreasmosti
- */
+ * 
+ * Try to keep the code dynamic, NO HARD CODING HERE 
+
+
+*/
 
 import org.jibble.pircbot.*;
 import java.util.ArrayList; 
@@ -23,12 +27,14 @@ public class mostiBot extends PircBot {
     
     public mostiBot(){
         this.setName("mostiBot");
-        options.add("time");
+        options.add("!time");
+        options.add("!users"); 
+        options.add("!uptime");
     }
     
     public void onMessage(String channel, String sender, String login, String hostname, String message) {
             
-           if (message.equalsIgnoreCase("time")) {
+           if (message.equalsIgnoreCase("!time")) {
             String time = new java.util.Date().toString();
             
             sendMessage(channel, sender + ": The time is now " + time);
@@ -38,16 +44,74 @@ public class mostiBot extends PircBot {
                 ut += options.get(i) + " "; 
             }
             sendMessage(channel, sender + " The options are:" + ut); 
-        }if(message.equalsIgnoreCase("users")) {
+        }if(message.equalsIgnoreCase("!users")) {
             User[] users = new User[getUsers(channel).length]; 
                 users = getUsers(channel); 
                 String ut = ""; 
                 for (int i = 0; i < users.length; i++) {
-                    ut += users[i].getNick(); 
+                    ut += users[i].getNick() + " "; 
          }
                 sendMessage(channel, sender + ": users: " + ut); 
+         }if(message.equalsIgnoreCase("!quit") && sender.equalsIgnoreCase("mosti")){
+             quitServer(sender + " killed me!"); 
+         
+         }if(message.equalsIgnoreCase("!uptime")){
+             try{
+                 String host = getHostName(); 
+                 String uptime = "";
+                 uptime = getBash("uptime")[0];
+                 sendMessage(channel, "Uptime for " + getHostName() + ": " +uptime); 
+             }catch(Exception e) { 
+                 System.out.println(e.getMessage());
+                 sendMessage(channel, e.getMessage()); 
+             }
          }
        }
+    
+    public String getHostName(){
+        String host = ""; 
+        try{
+            host = getBash("hostname")[0]; 
+            return host; 
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            host = e.getMessage();
+        }
+        return host; 
+    }	
         
-        
-    }
+    
+    
+    public String[] getBash(String command) {
+			String[] cmd = {"/bin/sh","-c",command};
+			String[] out = new String[10];
+			int i = 0;	
+                        try {
+				Process proc = Runtime.getRuntime().exec(cmd);
+				BufferedReader read = new BufferedReader(new InputStreamReader(
+						proc.getInputStream()));
+				
+				
+				out[i] = read.readLine();
+				while (read.ready()&&i<9) {
+					
+				out[i] = read.readLine();
+				i++;
+				}
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				
+			} 
+
+
+			return out;
+		}
+		
+	
+	}
+       
+    
+    
+    
+ 
